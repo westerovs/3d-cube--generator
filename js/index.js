38f07323ea14fadb4a0b42c2080f850fd1a7ecbe
westerovs <westerovs@mail.ru>
2020-12-19 13:27:29 +0300

    добавил в генератор низ и верх и крылья

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 (function () {
-    const container = document.querySelector('.jet');
+    const container = document.querySelector('.box');
 
-    let rotateY = 20;
-    let rotateX = -60;
+    let rotateY = 50;
+    let rotateX = 28;
 
     container.style.transform =
         `rotateY(${+rotateY}deg) rotateX(${+rotateX}deg) `
@@ -21,18 +21,20 @@
 
 class Create3dBox {
     constructor(
-        width  = 100,
-        height = 50
+        height = 50,
+        length = 200
     ) {
         // параметры
         this.height = height
-        this.length = 300
+        this.length = length
 
-        this.sides     = document.querySelectorAll('.side')
-        this.sideFront = document.querySelector('.side-front')
-        this.sideBack  = document.querySelector('.side-back')
-        this.sideRight = document.querySelector('.side-right')
-        this.sideLeft  = document.querySelector('.side-left')
+        this.sides      = document.querySelectorAll('.box__side')
+        this.sideFront  = document.querySelector('.box__side--front')
+        this.sideBack   = document.querySelector('.box__side--back')
+        this.sideRight  = document.querySelector('.box__side--right')
+        this.sideLeft   = document.querySelector('.box__side--left')
+        this.sideTop    = document.querySelector('.box__side--top')
+        this.sideBottom = document.querySelector('.box__side--bottom')
 
         this.translate = 50
         this.rotateY   = 90
@@ -45,8 +47,10 @@ class Create3dBox {
     }
 
     setLength = () => {
-        this.sideLeft.style.width  = `${ this.length}px`
-        this.sideRight.style.width = `${ this.length}px`
+        this.sideLeft.style.width    = `${this.length}px`
+        this.sideRight.style.width   = `${this.length}px`
+        this.sideTop.style.height    = `${this.length}px`
+        this.sideBottom.style.height = `${this.length}px`
 
         this.sideLeft.style.transform = `
             rotateY(   -${  this.rotateY }deg) 
@@ -58,11 +62,20 @@ class Create3dBox {
             translateX(${ (this.length / 2) - this.translate }px)
             translateZ(${ (this.length / 2) - (this.length - 100)}px)`
 
-        this.sideRight.style.background = 'red'
-
         this.sideBack.style.transform  = `
             translateZ(-${ this.length - this.translate}px)
              rotateY(180deg)`
+
+        this.sideTop.style.transform  = `
+             translateZ(-${ (this.length / 2) - 50}px)
+             translateY(-${ (this.length / 2) }px)
+             rotateX(90deg)`
+
+        this.sideBottom.style.transform  = `
+             translateZ(-${ (this.length / 2) - 50}px)
+             translateY(-${ (this.length / 2) - 50}px)
+             rotateX(90deg)`
+
     }
 
     init = () => {
