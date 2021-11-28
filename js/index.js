f3574d141e03cfc7ea1e2c216426fea49aaa9322
Александр <westerovs@mail.ru>
2021-11-28 03:19:32 +0300

    added axis

--- js/index.js
+++ js/index.js
@@ -1,72 +1,41 @@
 import { initControls } from './utils/controls.js'
 initControls()
 
-
-class Create3dBox {
-  constructor(
-    height = 50,
-    length = 175
-  ) {
+class Cube {
+  constructor(height) {
     // параметры
     this.height = height
-    this.length = length
-    
+
     this.sides = document.querySelectorAll('.box__side')
     this.sideFront = document.querySelector('.box__side--front')
     this.sideBack = document.querySelector('.box__side--back')
-    this.sideRight = document.querySelector('.box__side--right')
-    this.sideLeft = document.querySelector('.box__side--left')
     this.sideTop = document.querySelector('.box__side--top')
     this.sideBottom = document.querySelector('.box__side--bottom')
-    
-    this.translate = 50
-    this.rotateY = 90
-    
-    this.sideFront.style.transform = `translateZ(${ this.translate }px)`
+    this.sideLeft = document.querySelector('.box__side--left')
+    this.sideRight = document.querySelector('.box__side--right')
   }
-  
-  setHeight = () => {
-    this.sides.forEach(side => side.style.height = `${ this.height }px`)
-  }
-  
-  setLength = () => {
-    this.sideLeft.style.width = `${ this.length }px`
-    this.sideRight.style.width = `${ this.length }px`
-    this.sideTop.style.height = `${ this.length }px`
-    this.sideBottom.style.height = `${ this.length }px`
-    
-    this.sideLeft.style.transform = `
-            rotateY(   -${ this.rotateY }deg)
-            translateX(-${ (this.length / 2) - this.translate }px)
-            translateZ( ${ this.length / 2 }px)`
-    
-    this.sideRight.style.transform = `
-            rotateY(${ this.rotateY }deg)
-            translateX(${ (this.length / 2) - this.translate }px)
-            translateZ(${ (this.length / 2) - (this.length - 100) }px)`
-    
-    this.sideBack.style.transform = `
-            translateZ(-${ this.length - this.translate }px)
-             rotateY(180deg)`
-    
-    this.sideTop.style.transform = `
-             translateZ(-${ (this.length / 2) - 50 }px)
-             translateY(-${ (this.length / 2) }px)
-             rotateX(90deg)`
-    
-    this.sideBottom.style.transform = `
-             translateZ(-${ (this.length / 2) - 50 }px)
-             translateY(-${ (this.length / 2) - 50 }px)
-             rotateX(90deg)`
-    
-  }
-  
+
   init = () => {
-    this.setHeight()
-    this.setLength()
+    // this.sideTop.style.height = `${ this.height}px`
+    // this.sideBottom.style.height = `${ this.height}px`
+    // this.sideLeft.style.width = `${ this.height }px`
+    // this.sideRight.style.width = `${ this.height }px`
+
+    this.sideFront.style.transform = `translateZ(${ this.height / 2 }px)`
+    this.sideBack.style.transform = `translateZ(${ -this.height / 2 }px) rotateY(180deg)`
+
+    this.sideTop.style.transform = `translateZ(${ 0 }px) translateY(-${ this.height / 2 }px) rotateX(90deg)`
+    this.sideBottom.style.transform = `translateZ(${ 0 }px) translateY(${ this.height / 2 }px) rotateX(-90deg)`
+
+    this.sideLeft.style.transform = `translateZ(${ 0 }px) translateY(${ 0 }px) rotateX(0deg)`
+
+
+
+    // this.sideRight.style.transform = `
+    //         rotateY(${ this.rotateY }deg)
+    //         translateX(${ (this.length / 2) - this.translate }px)
+    //         translateZ(${ (this.length / 2) - (this.length - 100) }px)`
   }
-  
 }
 
-const create3dBox = new Create3dBox()
-create3dBox.init()
+new Cube(100).init()
--- js/utils/controls.js
+++ js/utils/controls.js
@@ -1,18 +1,18 @@
 const initControls = () => {
   const box = document.querySelector('.box')
-  let rotateY = 40
+  let rotateY = 20
   let rotateX = 40
   let rotateZ = 0
   
   box.style.transform = `rotateY(${ +rotateY }deg) rotateX(${ +rotateX }deg) `
   
   document.onkeydown = (key) => {
-    if (key.code === 'KeyW') rotateX += 2 // вверх
-    else if (key.code === 'KeyS') rotateX -= 2 // вниз
-    else if (key.code === 'KeyQ') rotateY -= 2 // вниз
-    else if (key.code === 'KeyE') rotateY += 2 // вниз
-    else if (key.code === 'KeyA') rotateZ -= 2     // влево
-    else if (key.code === 'KeyD') rotateZ += 2 // вправо
+    if (key.code === 'KeyW')      rotateX += 5 // вверх
+    else if (key.code === 'KeyS') rotateX -= 5 // вниз
+    else if (key.code === 'KeyQ') rotateY -= 5 // вниз
+    else if (key.code === 'KeyE') rotateY += 5 // вниз
+    else if (key.code === 'KeyA') rotateZ -= 5 // влево
+    else if (key.code === 'KeyD') rotateZ += 5 // вправо
     
     box.style.transform = `
       rotateY(${ +rotateY }deg)
