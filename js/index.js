68cbfea942f58e65523db08d916c4f123c6817f7
Александр <westerovs@mail.ru>
2021-11-28 03:42:02 +0300

    complete box v1

--- js/index.js
+++ js/index.js
@@ -6,30 +6,46 @@ class Cube {
     // параметры
     this.height = height
 
-    this.sides = document.querySelectorAll('.box__side')
-    this.sideFront = document.querySelector('.box__side--front')
-    this.sideBack = document.querySelector('.box__side--back')
-    this.sideTop = document.querySelector('.box__side--top')
-    this.sideBottom = document.querySelector('.box__side--bottom')
-    this.sideLeft = document.querySelector('.box__side--left')
-    this.sideRight = document.querySelector('.box__side--right')
+    this.container = document.querySelector('.container')
+    this.box = this.container.querySelector('.box')
+  
+    this.sides = this.box.querySelectorAll('.box__side')
+    this.sideFront = this.box.querySelector('.box__side--front')
+    this.sideBack = this.box.querySelector('.box__side--back')
+    this.sideTop = this.box.querySelector('.box__side--top')
+    this.sideBottom = this.box.querySelector('.box__side--bottom')
+    this.sideLeft = this.box.querySelector('.box__side--left')
+    this.sideRight = this.box.querySelector('.box__side--right')
   }
 
   init = () => {
-    // this.sideTop.style.height = `${ this.height}px`
-    // this.sideBottom.style.height = `${ this.height}px`
-    // this.sideLeft.style.width = `${ this.height }px`
-    // this.sideRight.style.width = `${ this.height }px`
-
+    this.setWidth()
+    this.setTranslate()
+  }
+  
+  setWidth = () => {
+    this.container.style.width = `${ this.height }px`
+    this.container.style.height = `${ this.height }px`
+    this.box.style.width = `${ this.height }px`
+    this.box.style.height = `${ this.height }px`
+    
+    this.sides.forEach(side => {
+      side.style.width = `${ this.height }px`
+      side.style.height = `${ this.height }px`
+    })
+  }
+  
+  setTranslate = () => {
     this.sideFront.style.transform = `translateZ(${ this.height / 2 }px)`
     this.sideBack.style.transform = `translateZ(${ -this.height / 2 }px) rotateY(180deg)`
-
+  
     this.sideTop.style.transform = `translateZ(${ 0 }px) translateY(-${ this.height / 2 }px) rotateX(90deg)`
     this.sideBottom.style.transform = `translateZ(${ 0 }px) translateY(${ this.height / 2 }px) rotateX(-90deg)`
-
+  
     this.sideLeft.style.transform = `translateX(-${ this.height / 2 }px) rotateY(-90deg)`
     this.sideRight.style.transform = `translateX(${ this.height / 2 }px) rotateY(90deg)`
   }
+  
 }
 
-new Cube(100).init()
+new Cube(200).init()
